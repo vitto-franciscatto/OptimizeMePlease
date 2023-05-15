03ef076f9ce283089d488d0c794f6926bada4001
Vitto Franciscatto <vitto.franciscatto@gmail.com>
2023-05-14 23:24:53 -0300

    :sparkles: feat: Optimize with Dapper and Slapper.AutoMapper

--- BenchmarkService.cs
+++ BenchmarkService.cs
@@ -1,11 +1,16 @@
 ﻿using BenchmarkDotNet.Attributes;
 using BenchmarkDotNet.Columns;
 using BenchmarkDotNet.Configs;
+using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
 using OptimizeMePlease.Context;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using Dapper;
+using Dapper.Contrib.Extensions;
+using OptimizeMePlease.Entities;
+using System.Threading.Tasks;
 
 namespace OptimizeMePlease
 {
@@ -128,6 +133,80 @@ namespace OptimizeMePlease
             authors.ForEach(_ => _.Books = _.Books.Where(b => b.PublishedYear < 1900));
 
             return authors;
+        }
+
+        [Benchmark]
+        public async Task<List<AuthorDTO_Optimized>> GetAuthors_Optimized_Dapper()
+        {
+            using var dbContext = new AppDbContext();
+
+            const string sql = @"
+                    SELECT
+	                    a.Id as Id,
+	                    a.Age as Age,
+	                    a.Country as Country,
+	                    a.BooksCount as BooksCount,
+	                    a.NickName as NickName,
+	                    a.UserId as UserId,
+	                    u.Id as User_Id,
+	                    u.FirstName as User_FirstName,
+	                    u.LastName as User_LastName,
+	                    u.UserName as User_UserName,
+	                    u.Email as User_Email,
+	                    u.Created as User_Created,
+	                    u.EmailConfirmed as User_EmailConfirmed,
+	                    u.LastActivity as User_LastActivity,
+	                    b.Id as Books_Id,
+	                    b.Name as Books_Name,
+	                    b.AuthorId as Books_AuthorId,
+	                    b.Published as Books_Published,
+	                    b.ISBN as Books_ISBN,
+	                    b.PublisherId as Books_PublisherId,
+	                    p.Id as Books_Published_Id,
+	                    p.Name as Books_Published_Name,
+	                    p.Established as Books_Published_Established
+                    FROM Authors a
+                        INNER JOIN Users u ON a.UserId = u.Id
+                        INNER JOIN Books b ON a.Id = b.AuthorId
+                        INNER JOIN Publishers p ON b.PublisherId = p.Id
+                        WHERE 
+                            a.Country = 'Serbia' 
+                        AND 
+                            a.Age = 27
+                        ORDER BY a.BooksCount DESC";
+            const string connString = "Server=(LocalDB)\\MSSQLLocalDB;Database=OptimizeMePlease;Trusted_Connection=True;Integrated Security=true;MultipleActiveResultSets=true";
+
+            using (var connection = new SqlConnection(connString))
+            {
+                var result = await connection.QueryAsync<dynamic>(sql);
+
+                Slapper.AutoMapper.Configuration.AddIdentifiers(typeof(Author), new List<string> { "Id" });
+                Slapper.AutoMapper.Configuration.AddIdentifiers(typeof(User), new List<string> { "Id" });
+                Slapper.AutoMapper.Configuration.AddIdentifiers(typeof(Book), new List<string> { "Id" });
+                Slapper.AutoMapper.Configuration.AddIdentifiers(typeof(Publisher), new List<string> { "Id" });
+
+                var authors_opt = Slapper.AutoMapper.MapDynamic<Author>(result)
+                    .Take(2)
+                    .Select(x => new AuthorDTO_Optimized
+                    {
+                        FirstName = x.User.FirstName,
+                        LastName = x.User.LastName,
+                        Email = x.User.Email,
+                        UserName = x.User.UserName,
+                        Books = x.Books.Select(y => new BookDTO_Optimized
+                        {
+                            Title = y.Name,
+                            PublishedYear = y.Published.Year
+                        }),
+                        Age = x.Age,
+                        Country = x.Country,
+                    })
+                    .ToList();
+
+                authors_opt.ForEach(_ => _.Books = _.Books.Where(b => b.PublishedYear < 1900));
+
+                return authors_opt;
+            }
         }
 
         //[Benchmark]
